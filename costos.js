aaddecfcdc94d1942c849944eff6acd5cd153b08
pizarro0823 <33677625+pizarro0823@users.noreply.github.com>
2020-03-16 09:12:55 -0500

    Update costos.js

--- costos.js
+++ costos.js
@@ -1,14 +1,14 @@
-var cabezal = parseInt("30500") ;
-var sillar = parseInt("35300") ;
-var jamba =parseInt("29000") ;
-var traslape =parseInt("20000") ;
-var enganche =parseInt("26000") ;
-var socalos =parseInt("29900") ;
-var rodamientos = parseInt("800") ;
+var cabezal = parseInt("30500");
+var sillar = parseInt("35300");
+var jamba = parseInt("29000");
+var traslape = parseInt("20000");
+var enganche = parseInt("26000");
+var socalos = parseInt("29900");
+var rodamientos = parseInt("800");
 var guias = parseInt("200");
-var empaque =parseInt("0") ;
-var inpbase = Number(document.getElementById('inpbase').value);
-var inpaltura = Number(document.getElementById('inpaltura').value);
+var empaque = parseInt("0");
+var inpbase = Number(document.getElementById('inpbase').value.replace(",", "."));
+var inpaltura = Number(document.getElementById('inpaltura').value.replace(",", "."));
 
 
 function calculuar() {
@@ -21,6 +21,15 @@ function calculuar() {
   document.getElementById('spanRodamientos').innerHTML = rodamientos.toFixed(0);
   document.getElementById('spanGuias').innerHTML = guias.toFixed(0);
   document.getElementById('spanEmpaque').innerHTML = empaque.toFixed(0);
+}
+
+function limpiar()
+{
+document.getElementById("inpbase").value="";
+document.getElementById("inpaltura").value="";
+
+
+
 }
 
 function costos() {
@@ -36,11 +45,11 @@ function costos() {
   var precio_guias = guias * 4;
   var precio_empaque = empaque * inpbase * inpaltura / 2;
 
-  var costo_por_medidas = precio_cabezal + precio_sillar + precio_jamba + precio_enganche + precio_traslape + precio_socalos+precio_rodamientos+precio_guias+precio_empaque;
+  var costo_por_medidas = precio_cabezal + precio_sillar + precio_jamba + precio_enganche + precio_traslape + precio_socalos + precio_rodamientos + precio_guias + precio_empaque;
 
-  var porcentaje_de_ganacia = precio_cabezal + precio_sillar + precio_jamba + precio_enganche + precio_traslape + precio_socalos+precio_rodamientos+precio_guias+precio_empaque * 60 / 100;
+  var porcentaje_de_ganacia = precio_cabezal + precio_sillar + precio_jamba + precio_enganche + precio_traslape + precio_socalos + precio_rodamientos + precio_guias + precio_empaque;
 
-  var total_venta =costo_por_medidas+porcentaje_de_ganacia;
+  var total_venta = costo_por_medidas + porcentaje_de_ganacia * 60 /100;
 
   var x = document.getElementById("mytabla1").rows[1].cells;
   x[2].innerHTML = Math.trunc(precio_cabezal).toFixed(0);
@@ -73,7 +82,7 @@ function costos() {
   x[1].innerHTML = "$ " + costo_por_medidas.toFixed(0);
 
   var x = document.getElementById("mytabla1").rows[12].cells;
-  x[1].innerHTML = "$ " + porcentaje_de_ganacia.toFixed(0);
+  x[1].innerHTML = "$ " + porcentaje_de_ganacia * 60 / 100 .toFixed(0);
 
   var x = document.getElementById("mytabla1").rows[13].cells;
   x[1].innerHTML = "$ " + total_venta.toFixed(0);
@@ -91,26 +100,28 @@ function costos() {
   var pempaque = inpbase * inpaltura / 2;
 
   var x = document.getElementById("mytabla1").rows[1].cells;
-  x[1].innerHTML = pcabezal.toFixed(1);
+  x[1].innerHTML = pcabezal.toFixed(2);
   var x = document.getElementById("mytabla1").rows[2].cells;
-  x[1].innerHTML = psillar.toFixed(1);
+  x[1].innerHTML = psillar.toFixed(2);
   var x = document.getElementById("mytabla1").rows[3].cells;
-  x[1].innerHTML = pjamba.toFixed(1);
+  x[1].innerHTML = pjamba.toFixed(2);
   var x = document.getElementById("mytabla1").rows[4].cells;
-  x[1].innerHTML = penganche.toFixed(1);
+  x[1].innerHTML = penganche.toFixed(2);
   var x = document.getElementById("mytabla1").rows[5].cells;
-  x[1].innerHTML = ptraslape.toFixed(1);
+  x[1].innerHTML = ptraslape.toFixed(2);
   var x = document.getElementById("mytabla1").rows[6].cells;
-  x[1].innerHTML = psocalos.toFixed(1);
+  x[1].innerHTML = psocalos.toFixed(2);
   var x = document.getElementById("mytabla1").rows[7].cells;
   x[1].innerHTML = prodamientos.toFixed(0);
   var x = document.getElementById("mytabla1").rows[8].cells;
   x[1].innerHTML = pguias.toFixed(0);
   var x = document.getElementById("mytabla1").rows[9].cells;
-  x[1].innerHTML = pempaque.toFixed(1);
+  x[1].innerHTML = pempaque.toFixed(2);
 
 
 }
+
+
 
 
 
